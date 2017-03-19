0ab304be8bf48dd1a59503480b073a9fe9b95ffb
Christoph Dorn <christoph@christophdorn.com>
2017-03-18 23:00:48 -0700

    wip

--- _#_org.bashorigin_#_s1.sh
+++ _#_org.bashorigin_#_s1.sh
@@ -3,7 +3,7 @@
 function EXPORTS_expect {
     local status=$(curl --write-out %{http_code} --silent --output /dev/null "${2}")
     if [ "$status" != "${1}" ]; then
-        echo "ERROR: Not Up! Got status $status for url ${2}."
+        echo "ERROR: Got status $status for url ${2} while expecting ${1}."
         exit 1
     fi
 }
